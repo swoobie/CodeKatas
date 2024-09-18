94fe2f6df2ebab7af539fe31e44d76202f3c7d52
Jared Ross <jdross91@gmail.com>
2024-09-18 09:27:40 -0600

    9/16 kata

--- KataTests.cs
+++ KataTests.cs
@@ -1,5 +1,33 @@
 namespace CodeKatas;
 
 public class Tests {
-    [
+    
+[Test]
+public void ContainsPythagoreanTriplet_ValidTriplet_ReturnsTrue() {
+    int[] input = [3, 4, 5];
+    var result = Katas.ContainsPythagoreanTriplet(input);
+    Assert.That(result, Is.True);
+}
+
+[Test]
+public void ContainsPythagoreanTriplet_NoTriplet_ReturnsFalse() {
+    int[] input = [1, 2, 3];
+    var result = Katas.ContainsPythagoreanTriplet(input);
+    Assert.That(result, Is.False);
+}
+
+[Test]
+public void ContainsPythagoreanTriplet_EmptyArray_ReturnsFalse() {
+    int[] input = [];
+    var result = Katas.ContainsPythagoreanTriplet(input);
+    Assert.IsFalse(result);
+}
+
+[Test]
+public void ContainsPythagoreanTriplet_LongTripletList_ReturnsTrue() {
+    int[] input = [10, 6, 8, 10, 9, 12];
+    var result = Katas.ContainsPythagoreanTriplet(input);
+    Assert.That(result, Is.True);
+}
+    
 }
--- Katas.cs
+++ Katas.cs
@@ -1,5 +1,37 @@
 ﻿namespace CodeKatas;
 
 public class Katas {
-    
+ 
+/*
+* 9/18/2024
+* Given an array of integers, write a function to determine if it contains a Pythagorean triplet.
+* A Pythagorean triplet consists of three integers (a, b, c) such that: a² + b² = c²
+*/
+    public static bool ContainsPythagoreanTriplet(int[] arr) {
+        // square all elements
+        for (int i = 0; i < arr.Length; i++) {
+            arr[i] = arr[i] * arr[i];
+        }
+        
+        // sort them
+        Array.Sort(arr);
+        
+        // check if there is a triplet
+        for (int i = arr.Length - 1; i >= 2; i--) {
+            var left = 0;
+            var right = i - 1;
+            while (left < right) {
+                if (arr[left] + arr[right] == arr[i]) {
+                    return true;
+                }
+                if (arr[left] + arr[right] < arr[i]) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return false;
+    }
 }
